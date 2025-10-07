4075f4e37bdacc6de32367697ddfee555eecf673
elliotechne <elliotechne42@gmail.com>
2025-10-07 11:42:37 -0400

    fix: skip requesting account id

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,6 +9,8 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
+
 
     # Access credentials should be passed via:
     # - Backend config flags during init
