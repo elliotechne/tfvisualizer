9fcb8d3eb3006b9fe322c2e04595e9563994bf8a
elliotechne <elliotechne42@gmail.com>
2025-10-07 12:14:59 -0400

    fix: terraform fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -10,7 +10,7 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    access_key                  = "foo" 
+    access_key                  = "foo"
     secret_key                  = "bar"
 
 
