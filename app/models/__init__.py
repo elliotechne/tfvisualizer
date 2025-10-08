d4e5a5538e716fdf6ea801870a2d498f24d19d1b
elliotechne <elliotechne42@gmail.com>
2025-10-08 18:03:33 -0400

    fix: registration issue.

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.user import User
+from app.models.project import Project
+from app.models.subscription import Subscription
+from app.models.payment import PaymentHistory
+
+__all__ = ['User', 'Project', 'Subscription', 'PaymentHistory']
