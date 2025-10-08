0b11e3bbc7c818a83c021b01c15e2e02ffb1ee24
elliotechne <elliotechne42@gmail.com>
2025-10-08 13:58:02 -0400

    fix: token not found error.

--- terraform/external-dns.tf
+++ terraform/external-dns.tf
@@ -1,3 +1,26 @@
+# Secret for external-dns DigitalOcean token
+resource "kubernetes_secret" "external_dns_token" {
+  metadata {
+    name      = "external-dns-token"
+    namespace = "external-dns"
+  }
+
+  data = {
+    token = var.do_token
+  }
+
+  type = "Opaque"
+
+  depends_on = [kubernetes_namespace.external_dns]
+}
+
+# Namespace for external-dns
+resource "kubernetes_namespace" "external_dns" {
+  metadata {
+    name = "external-dns"
+  }
+}
+
 # External DNS for automatic DNS management
 resource "helm_release" "external_dns" {
   name       = "external-dns"
@@ -6,7 +29,7 @@ resource "helm_release" "external_dns" {
   namespace  = "external-dns"
   version    = "1.14.3"
 
-  create_namespace = true
+  create_namespace = false
 
   set {
     name  = "provider"
@@ -14,8 +37,18 @@ resource "helm_release" "external_dns" {
   }
 
   set {
-    name  = "digitalocean.apiToken"
-    value = var.do_token
+    name  = "env[0].name"
+    value = "DO_TOKEN"
+  }
+
+  set {
+    name  = "env[0].valueFrom.secretKeyRef.name"
+    value = kubernetes_secret.external_dns_token.metadata[0].name
+  }
+
+  set {
+    name  = "env[0].valueFrom.secretKeyRef.key"
+    value = "token"
   }
 
   set {
@@ -62,4 +95,6 @@ resource "helm_release" "external_dns" {
     name  = "logFormat"
     value = "json"
   }
+
+  depends_on = [kubernetes_secret.external_dns_token]
 }
