dd85c6bbf6a722b1d2375413d923f1ed53e2e75c
elliotechne <elliotechne42@gmail.com>
2025-10-27 11:05:16 -0400

    feat: added vpa helm chart

--- terraform/goldilocks.tf
+++ terraform/goldilocks.tf
@@ -6,3 +6,13 @@ resource "helm_release" "goldilocks" {
   version          = "10.1.0"
   create_namespace = true
 }
+
+resource "helm_release" "vpa" {
+  name             = "vpa"
+  repository       = "https://charts.fairwinds.com/stable"
+  chart            = "vpa"
+  namespace        = "fairwinds-stable"
+  version          = "4.9.0"
+  create_namespace = true
+}
+
