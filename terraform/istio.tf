e3406a96da58917aa29eae852a0d17838202be37
elliotechne <elliotechne42@gmail.com>
2026-03-01 12:51:32 -0500

    fix: lb name

--- terraform/istio.tf
+++ terraform/istio.tf
@@ -37,7 +37,7 @@ resource "helm_release" "istio_ingressgateway" {
       service = {
         type = "LoadBalancer"
         annotations = {
-          "service.beta.kubernetes.io/do-loadbalancer-name" = "${var.project_name}-${var.environment}-lb"
+          "service.beta.kubernetes.io/do-loadbalancer-name" = "${var.project_name}-${var.environment}-istio-lb"
           "external-dns.alpha.kubernetes.io/hostname"       = "${var.domain_name},www.${var.domain_name}"
         }
       }
