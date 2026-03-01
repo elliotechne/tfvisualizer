d73a55157bafbdc2aba3e001e3e4bd0b7699573a
elliotechne <elliotechne42@gmail.com>
2026-03-01 17:04:53 -0500

    feat: switch to letsencrypt

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -26,20 +26,6 @@ resource "time_sleep" "wait_for_cert_manager" {
   create_duration = "60s"
 }
 
-# Secret containing the ZeroSSL EAB HMAC key
-resource "kubernetes_secret" "zerossl_eab" {
-  metadata {
-    name      = "zerossl-eab-secret"
-    namespace = "cert-manager"
-  }
-
-  data = {
-    secret = var.zerossl_eab_hmac_key
-  }
-
-  depends_on = [helm_release.cert_manager]
-}
-
 # DigitalOcean API token secret for cert-manager DNS01 solver
 resource "kubernetes_secret" "do_dns_cert_manager" {
   metadata {
@@ -54,24 +40,19 @@ resource "kubernetes_secret" "do_dns_cert_manager" {
   depends_on = [helm_release.cert_manager]
 }
 
-# ZeroSSL Production ClusterIssuer using DNS01 via DigitalOcean
-resource "kubectl_manifest" "zerossl_cluster_issuer" {
+# Let's Encrypt Production ClusterIssuer using DNS01 via DigitalOcean
+resource "kubectl_manifest" "letsencrypt_cluster_issuer" {
   yaml_body = <<-YAML
     apiVersion: cert-manager.io/v1
     kind: ClusterIssuer
     metadata:
-      name: zerossl-prod
+      name: letsencrypt-prod
     spec:
       acme:
-        server: https://acme.zerossl.com/v2/DV90
+        server: https://acme-v02.api.letsencrypt.org/directory
         email: ${var.letsencrypt_email}
         privateKeySecretRef:
-          name: zerossl-prod-account-key
-        externalAccountBinding:
-          keyID: ${var.zerossl_eab_kid}
-          keySecretRef:
-            name: zerossl-eab-secret
-            key: secret
+          name: letsencrypt-prod-account-key
         solvers:
         - dns01:
             digitalocean:
@@ -82,7 +63,6 @@ resource "kubectl_manifest" "zerossl_cluster_issuer" {
 
   depends_on = [
     time_sleep.wait_for_cert_manager,
-    kubernetes_secret.zerossl_eab,
     kubernetes_secret.do_dns_cert_manager,
   ]
 }
